7d4b1d22c6cd67e59fe290b3c3f7e942c2ef8bd8
Matteo <pro.matteo.piselli@gmail.com>
2025-05-30 00:33:51 +0200

    refactor: réécriture du composant Home avec ajout de la recherche de joueur et récupération de la dernière version du patch

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,15 +1,183 @@
-import styles from '../styles/Home.module.css';
+// Import Next
+import Image from "next/image";
+// Autres imports
+import { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
-function Home() {
+export default function Home() {
+  const [region, setRegion] = useState(false);
+  const [username, setUsername] = useState("");
+  const [tagLine, setTagLine] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [isHistoryVisible, setIsHistoryVisible] = useState(false);
+  const [playerData, setPlayerData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [latestPatch, setLatestPatch] = useState("");
+
+  const toggleDropdown = () => setRegion(!region);
+
+  // ---------- Search player function ---------- //
+  const searchPlayer = async () => {
+    if (username && tagLine) {
+      setIsLoading(true);
+      // ---------- Search player ---------- //
+      try {
+        const response = await fetch(
+          `http://localhost:3000/summoner/${username}/${tagLine}`
+        );
+        const data = await response.json();
+        console.log("Player data:", data);
+        setPlayerData(data);
+      } catch (error) {
+        console.error("Error fetching player data:", error);
+        setPlayerData(null);
+      } finally {
+        // ------- Reset input value after search ------- //
+        setInputValue("");
+        setUsername("");
+        setTagLine("");
+        setIsLoading(false);
+      }
+    } else {
+      console.error("Please enter a valid username and tag line.");
+    }
+  };
+
+  // ---------- Get lasted patch version ---------- //
+  const getLatestPatchVersion = async () => {
+    try {
+      const response = await fetch(
+        "https://ddragon.leagueoflegends.com/api/versions.json"
+      );
+      const versions = await response.json();
+      setLatestPatch(versions[0]); // Get the latest patch version
+    } catch (error) {
+      console.error("Error fetching patch version:", error);
+      return null;
+    }
+  };
+
+  // ---------- Fetch latest patch version on component mount ---------- //
+  useEffect(() => {
+    getLatestPatchVersion();
+  }, []);
+
   return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+    <div className="relative bg-[#111111] min-h-screen">
+      <div className="absolute inset-0 bg-[#222222] w-full min-h-screen container rounded-2xl overflow-y-auto mx-auto text-white">
+        <div className="flex justify-center mt-8">
+          {/* ----- Icon Faker -----*/}
+          <Image
+            src="/assets/op-gg-faker.png"
+            alt="icon faker"
+            width={392}
+            height={205}
+          />
+        </div>
+
+        {/* ----- Region and Username Input -----*/}
+        <div className="relative flex flex-row items-start justify-center mt-12">
+          {/* Button for region selection */}
+          <button
+            onClick={toggleDropdown}
+            className="w-32 bg-gray-700 text-white py-2"
+          >
+            EUW
+            {/* -------- Dropdown for regions -------*/}
+            <AnimatePresence>
+              {region && (
+                <motion.div
+                  className="absolute top-full w-32 bg-gray-700"
+                  initial={{ opacity: 0, y: -10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -10 }}
+                  transition={{ duration: 0.3 }}
+                >
+                  <ul className="py-1">
+                    <li className=" border-b border-black px-4 py-2 hover:bg-gray-800 cursor-pointer">
+                      Region 1
+                    </li>
+                    <li className="border-b border-black px-4 py-2 hover:bg-gray-800 cursor-pointer">
+                      Region 2
+                    </li>
+                    <li className="border-b border-black px-4 py-2 hover:bg-gray-800 cursor-pointer">
+                      Region 3
+                    </li>
+                  </ul>
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </button>
+
+          {/* ------- Input for username and tag line ---------*/}
+
+          <div className="relative flex flex-col items-center">
+            <input
+              type="text"
+              placeholder="Username#tagLine"
+              className="bg-gray-700 w-96 text-white p-2"
+              value={inputValue}
+              onChange={(e) => {
+                const value = e.target.value;
+                setInputValue(value);
+
+                if (value.includes("#")) {
+                  // Split the input into username and tag line
+                  const [name, tag] = value.split("#");
+                  setUsername(name || "");
+                  setTagLine(tag || "");
+                } else {
+                  // If no tag line, set username and clear tag line
+                  setUsername(value);
+                  setTagLine("");
+                }
+              }}
+              /* View history of searches */
+              onClick={() => setIsHistoryVisible(!isHistoryVisible)}
+            />
+          </div>
+
+          {/* ------- Button to search player -------*/}
+          <button
+            onClick={() => searchPlayer()}
+            className="bg-gray-700 px-4 py-2"
+          >
+            Search
+          </button>
+        </div>
+
+        {isLoading ? (
+          // ------ Loader while fetching data ----- //
+          <div className="flex justify-center items-center h-96">
+            <div className="loader border-t-4 border-b-4 border-blue-500 rounded-full w-12 h-12 animate-spin"></div>
+            <p className="ml-4 text-white">Chargement des données...</p>
+          </div>
+        ) : (
+          // ------ Display player data ----- //
+          playerData && (
+            <div className="flex items-center">
+              <div className="relative mt-8 ml-24 h-[125px] max-w-[110px]">
+                <Image
+                  src={`https://ddragon.leagueoflegends.com/cdn/${latestPatch}/img/profileicon/${playerData.summoner.profileIconId}.png`}
+                  alt="Profile Icon"
+                  width={110}
+                  height={110}
+                  className="rounded-xl"
+                />
+                <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-gray-900 text-white rounded-xl py-1 px-2 text-sm">
+                  {playerData.summoner.summonerLevel}
+                </div>
+              </div>
+              <div className="ml-4 text-xl">
+                <span className="font-bold">{playerData.riotId.gameName}</span>
+                <span className="text-gray-500">
+                  #{playerData.riotId.tagLine}
+                </span>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </div>
   );
 }
-
-export default Home;
