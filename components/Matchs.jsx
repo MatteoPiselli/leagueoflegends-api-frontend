bc3ffe708427f1b1cf9499cabbce0912da251aad
Matteo <pro.matteo.piselli@gmail.com>
2025-06-18 01:54:30 +0200

    amélioration de l'affichage des données de match et ajout de styles pour une meilleure présentation

--- components/Matchs.jsx
+++ components/Matchs.jsx
@@ -1,19 +1,40 @@
 export default function Matchs({ matchData }) {
   if (!matchData || matchData.length === 0) {
-    return <div>No match data available</div>;
+    return (
+      <div className="w-full flex justify-center items-center border border-[#dd1029] mt-8">
+        No match data available
+      </div>
+    );
   }
 
+  const queueId = {
+    400: "Normal Draft",
+    420: "Ranked Solo/Duo",
+    430: "Normal Blind Pick",
+    440: "Ranked Flex",
+    450: "ARAM",
+    700: "Clash",
+    720: "ARAM Clash",
+    900: "AURF",
+    1090: "Teamfight Tactics",
+    1100: "Ranked TFT",
+    1700: "Arena",
+    1710: "Arena",
+    2000: "Tutorial",
+    2010: "Tutorial",
+    2020: "Tutorial",
+  };
+
   return (
-    <div className="mt-8">
+    <div className="w-full flex flex-col border border-[#dd1029] rounded-lg mt-8">
       {matchData.map((match) => (
         <div
           key={match.matchDetails.metadata.matchId}
-          className="bg-[#0D1520] rounded-lg p-4 mb-4"
+          className="bg-[#19191B] rounded-lg p-4 mb-4"
         >
           <h3 className="border-b border-gray-300 pb-4">
-            Match ID: {match.matchDetails.metadata.matchId}
+            {queueId[match.matchDetails.info.queueId] || "Unknown Queue"}
           </h3>
-          <p>Game Mode: {match.matchDetails.info.gameMode}</p>
           <p>
             Duration: {Math.floor(match.matchDetails.info.gameDuration / 60)}{" "}
             minutes
