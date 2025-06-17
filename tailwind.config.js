b22e4125c893e1466067f37d87e9f36efb994ee1
Matteo <pro.matteo.piselli@gmail.com>
2025-06-18 01:53:36 +0200

    configuration du plugin tailwind-scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+const { plugin } = require("postcss");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  plugins: [require("tailwind-scrollbar")],
   theme: {
     extend: {},
   },
