95fd499a1c7cc34b57ff498551ec8eb8450401d6
Matteo Piselli <pro.matteo.piselli@gmail.com>
2025-09-17 19:50:57 +0200

    refactor: ajout de "flagcdn.com" à la liste des domaines d'images dans la configuration Next.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["ddragon.leagueoflegends.com", "raw.communitydragon.org"],
+    domains: [
+      "ddragon.leagueoflegends.com",
+      "raw.communitydragon.org",
+      "flagcdn.com",
+    ],
   },
 };
 
