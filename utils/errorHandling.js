3cbf6e36f3cd66dff93d9f82b181c1a8096fd233
Matteo Piselli <pro.matteo.piselli@gmail.com>
2025-09-19 12:05:35 +0200

    refactor: simplification de la gestion des messages d'erreur HTTP dans la fonction handleHttpError

--- utils/errorHandling.js
+++ utils/errorHandling.js
@@ -1,33 +1,31 @@
 // Function to handle HTTP errors
 export const handleHttpError = (status, statusText) => {
-  let message;
-
   switch (status) {
     case 400:
-      message = "Bad Request: Please check your input.";
+      statusText = "Bad Request: Please check your input.";
       break;
     case 401:
-      message = "Unknown API key: Please check your API key.";
+      statusText = "Unknown API key: Please check your API key.";
       break;
     case 404:
-      message = "Player not found. Please check the username and tag.";
+      statusText = "Player not found. Please check the username and tag.";
       break;
     case 403:
-      message =
+      statusText =
         "Forbidden: You do not have permission to access this resource. Please check your API key";
       break;
     case 429:
-      message =
+      statusText =
         "Too Many Requests. You have exceeded the rate limit for this API. Please try again in 1 minute.";
       break;
     case 500:
-      message = "Internal Server Error. Please try again later.";
+      statusText = "Internal Server Error. Please try again later.";
       break;
     default:
-      message = `Unknown error (Code ${status}). Please try again later.`;
+      statusText = `Unknown error (Code ${status}). Please try again later.`;
   }
 
-  console.error(`HTTP Error ${status}: ${message}`);
-  alert(`Error: ${status}${"\n\n"} ${message}`);
+  console.error(`HTTP Error ${status}: ${statusText}`);
+  alert(`Error: ${status}${"\n\n"} ${statusText}`);
   return false;
 };
