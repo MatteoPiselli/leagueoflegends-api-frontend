26d0ad1cb82d749ce6449008c84f0ecfc93e75d3
MatteoPiselli <pro.matteo.piselli@gmail.com>
2025-09-19 22:37:50 +0200

    feat: ajout des mappages d'ID pour les modes de jeu League of Legends

---
+++ constants/queueTypes.js
@@ -0,0 +1,18 @@
+// Queue ID mappings for League of Legends game modes
+export const QUEUE_TYPES = {
+  400: "Normal Draft",
+  420: "Ranked Solo/Duo",
+  430: "Normal Blind Pick",
+  440: "Ranked Flex",
+  450: "ARAM",
+  700: "Clash",
+  720: "ARAM Clash",
+  900: "AURF",
+  1090: "Teamfight Tactics",
+  1100: "Ranked TFT",
+  1700: "Arena",
+  1710: "Arena",
+  2000: "Tutorial",
+  2010: "Tutorial",
+  2020: "Tutorial",
+};
