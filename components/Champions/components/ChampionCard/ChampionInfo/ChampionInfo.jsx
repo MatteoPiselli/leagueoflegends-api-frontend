a3743c9750af107038c07b62b55a5ac159d54f07
MatteoPiselli <pro.matteo.piselli@gmail.com>
2025-10-02 01:26:11 +0200

    refactor: amélioration de la présentation du composant ChampionInfo avec des ajustements de taille et de mise en page

--- components/Champions/components/ChampionCard/ChampionInfo/ChampionInfo.jsx
+++ components/Champions/components/ChampionCard/ChampionInfo/ChampionInfo.jsx
@@ -1,23 +1,29 @@
 import Image from "next/image";
 
-export const ChampionInfo = ({ champion, latestPatch, getChampionName }) => (
-  <div className="flex items-center space-x-3">
-    <div className="w-12 h-12 rounded-lg overflow-hidden">
-      <Image
-        src={`https://ddragon.leagueoflegends.com/cdn/${latestPatch}/img/champion/${getChampionName(
-          champion.championId
-        )}.png`}
-        alt={champion.championName}
-        width={48}
-        height={48}
-        className="scale-[1.2]"
-      />
+export const ChampionInfo = ({ champion, latestPatch, getChampionName }) => {
+  console.log(" champion ", champion);
+  return (
+    <div className="flex items-center w-32 space-x-2">
+      <div className="w-8 h-8 rounded-lg overflow-hidden">
+        <Image
+          src={`https://ddragon.leagueoflegends.com/cdn/${latestPatch}/img/champion/${getChampionName(
+            champion.championId
+          )}.png`}
+          alt={champion.championName}
+          width={32}
+          height={32}
+          className="scale-[1.2]"
+        />
+      </div>
+      <div className="w-2/3">
+        <h3 className="text-white font-medium text-sm">
+          {getChampionName(champion.championId)}
+        </h3>
+        <p className="text-gray-400 text-xs">
+          <span>{champion.averageStats.cs} CS</span>
+          <span> ({champion.averageStats.csPerMinute})</span>
+        </p>
+      </div>
     </div>
-    <div>
-      <h3 className="text-white font-medium text-sm">
-        {champion.championName}
-      </h3>
-      <p className="text-gray-400 text-xs">{champion.games} games</p>
-    </div>
-  </div>
-);
+  );
+};
