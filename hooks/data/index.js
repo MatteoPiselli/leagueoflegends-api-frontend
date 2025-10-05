737109565652d0c917584d70e91ae941c584b4ab
MatteoPiselli <pro.matteo.piselli@gmail.com>
2025-10-05 02:41:59 +0200

    fix: suppression de l'export du hook usePlayerProfile dans le fichier index.js

--- hooks/data/index.js
+++ hooks/data/index.js
@@ -1,5 +1,4 @@
 // Data hooks - API data management
-export { usePlayerProfile } from "./usePlayerProfile";
 export { useRankedData } from "./useRankedData";
 export { useMatchData } from "./useMatchData";
 export { useMasteriesData } from "./useMasteriesData";
