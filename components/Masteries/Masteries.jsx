f67a9568b56835526145ce26ecb87e599e8ead6b
MatteoPiselli <pro.matteo.piselli@gmail.com>
2025-10-13 21:00:45 +0200

    fix: ajuster la mise en page et les propriétés des cartes de maîtrise

--- components/Masteries/Masteries.jsx
+++ components/Masteries/Masteries.jsx
@@ -14,11 +14,11 @@ export default function Masteries({
   return (
     <div className="h-fit w-full mt-8 p-4 bg-[#19191B] rounded-lg">
       {/* ---------- Header ---------- */}
-      <div className="flex items-center justify-between mb-6 pb-3 border-b border-[#dd1029]">
+      <div className="flex items-center justify-between mb-4 pb-3 border-b border-[#dd1029]">
         <h2 className="text-base font-semibold text-white">Masteries</h2>
       </div>
       {/* ---------- Masteries List ---------- */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-3 gap-6">
         {masteriesData.map((mastery) => (
           <MasteryCard
             key={mastery.championId}
--- components/Masteries/components/MasteryCard/MasteryCard.jsx
+++ components/Masteries/components/MasteryCard/MasteryCard.jsx
@@ -11,17 +11,13 @@ export const MasteryCard = ({
 
   const championName = getChampionName(mastery.championId);
   const championId = getChampionId(mastery.championId);
-  const masteryLevel = mastery.championLevel;
-  const masteryPoints = mastery.championPoints;
-  const banner =
-    "https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-shared-components/global/default/images/banner-primary.png";
 
   return (
     <div className="relative flex flex-col items-center p-4 rounded-lg min-w-0">
       {/* ---------- Champion Banner ---------- */}
       <div className="absolute inset-0 h-full w-full">
         <Image
-          src={banner}
+          src={`https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-shared-components/global/default/images/banner-primary.png`}
           alt={`${championName} banner`}
           width={100}
           height={270}
@@ -40,20 +36,20 @@ export const MasteryCard = ({
         {championName}
       </p>
       {/* ---------- Mastery Icon & Level ---------- */}
-      <div className="flex flex-col items-center mb-2 -space-y-1">
+      <div className="flex flex-col items-center mb-2 -space-y-1 z-10">
         <Image
-          src={getMasteryIconUrl(masteryLevel)}
-          alt={`Mastery Level ${masteryLevel}`}
+          src={getMasteryIconUrl(mastery.championLevel)}
+          alt={`Mastery Level ${mastery.championLevel} icon`}
           width={40}
           height={40}
         />
         <p className="text-xs text-gray-400 text-center bg-[#19191B] rounded-lg p-1 z-10">
-          {masteryLevel}
+          {mastery.championLevel}
         </p>
       </div>
       {/* ---------- Mastery Points ---------- */}
       <p className="text-xs text-gray-400 text-center mb-2 z-10">
-        {formatMasteryPoints(masteryPoints)} Pts
+        {formatMasteryPoints(mastery.championPoints)} Pts
       </p>
     </div>
   );
