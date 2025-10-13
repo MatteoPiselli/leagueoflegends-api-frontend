c5f70f62efee6f61610e0de9d9f286fac7c8396b
MatteoPiselli <pro.matteo.piselli@gmail.com>
2025-10-13 20:28:01 +0200

    feat: ajouter l'exportation de getChampionId dans le composant ChampionCard

--- components/Champions/components/ChampionCard/ChampionCard.jsx
+++ components/Champions/components/ChampionCard/ChampionCard.jsx
@@ -1,13 +1,19 @@
 import { ChampionInfo } from "./ChampionInfo";
 import { ChampionStats } from "./ChampionStats";
 
-export const ChampionCard = ({ champion, latestPatch, getChampionName }) => {
+export const ChampionCard = ({
+  champion,
+  latestPatch,
+  getChampionName,
+  getChampionId,
+}) => {
   return (
     <div className="flex items-center justify-between p-3 bg-[#1e1e20] rounded-lg">
       <ChampionInfo
         champion={champion}
         latestPatch={latestPatch}
         getChampionName={getChampionName}
+        getChampionId={getChampionId}
       />
 
       <ChampionStats champion={champion} />
