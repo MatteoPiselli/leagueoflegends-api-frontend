963f3b75062d9a20e0cf166cd1687cae1d010fa7
MatteoPiselli <pro.matteo.piselli@gmail.com>
2025-10-24 22:30:51 +0200

    refactor: supprimer le fournisseur Redux du composant App

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,24 +1,14 @@
 import "../styles/globals.css";
 import Head from "next/head";
 
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import summoner from "../reducers/summoner";
-
-const store = configureStore({
-  reducer: {
-    summoner,
-  },
-});
-
 function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
+    <>
       <Head>
         <title>SummonerFinder.gg</title>
       </Head>
       <Component {...pageProps} />
-    </Provider>
+    </>
   );
 }
 
