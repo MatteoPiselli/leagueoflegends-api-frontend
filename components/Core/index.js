564fe4d283e918268be652afaab6f3df03ef5ba3
Matteo <pro.matteo.piselli@gmail.com>
2025-11-22 03:09:17 +0100

    feat: ajouter le composant ApiLimitBanner pour informer des limitations de l'API

--- components/Core/index.js
+++ components/Core/index.js
@@ -3,3 +3,6 @@ export { SearchForm, PlayerProfile } from "./components";
 
 // State components
 export { ErrorState, LoadingState, WelcomeState } from "./States";
+
+// Informational components
+export { default as ApiLimitBanner } from "./ApiLimitBanner";
